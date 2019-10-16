82a842a5d487324698d0118d43e6fe646d3915e1
Katherine Chao <kchao@broadinstitute.org>
2019-10-16 14:56:36 -0400

    Updated regional_constraint.py to infer build (rather than making build an argument)

--- regional_constraint.py
+++ regional_constraint.py
@@ -1,4 +1,5 @@
-import hail as hl
+from gnomad_hail.utils.generic import get_reference_genome
+from gnomad_hail.utils.slack import *
 from regional_missense_constraint.resources.basics import *
 
 
@@ -10,42 +11,55 @@ logger.setLevel(logging.INFO)
 def main(args):
 
     if args.pre_process_data:
-        logger.info('Preprocessing reference fasta and gnomAD files')
-        pre_process_data(args.build)
+        logger.info('Preprocessing reference fasta and gencode files')
+        process_context_ht(args.build, args.trimers)
 
-    #full_genome_ht = prepare_ht(get_processed_genomes_ht_path(), args.trimers) 
-    #full_exome_ht = prepare_ht(get_processed_exomes_ht_path(), args.trimers)
-    #full_genome_ht = prepare_ht(f'{FLAGSHIP_LOF}/model/genomes_processed.ht', args.trimers)
-    full_exome_ht = prepare_ht(hl.read_table(f'{FLAGSHIP_LOF}/model/exomes_processed.ht'), args.trimers)
+        logger.info('Preprocessing gencode gtf information')
+        gencode_ht = process_gencode_ht(args.build)
 
+        logger.info('Done preprocessing files')
+
+    logger.info('Reading in input files')         
+    context_ht = hl.read_table(get_processed_context_ht_path(args.build))
+    exome_ht = prepare_ht(hl.read_table(processed_exomes_ht_path), args.trimers)
+    gencode_ht = get_processed_gencode_ht(args.build)
+
+    logger.info('Inferring build of exome ht')
+    rg = get_reference_genome(exome_ht.locus).name
+    contigs = rg.contigs[21]
+    logger.info('Annotating transcripts in context with divergence scores')
+    context_ht = context_ht.annotate(div=(hl.cond(
+                                                context_ht.div_scores.contains(context_ht.vep.transcript_consequences.transcript_id),
+                                                context_ht.div_scores[context_ht.vep.transcript_consequences.transcript_id],
+                                                0.0564635)))
+
+    # total rows in context_ht:  4014886371
+    # total rows in context_ht after filtering to missenses in protein coding, canonical transcripts: 92683899 
+    # total rows scored with average divergence score (=not in div_scores): 49443864
+    
     if args.test:
-        logger.info('Filtering to chr21 for testing')
-        if args.build == 'GRCh37':
-            contig = '21'
-        else:
-            contig = 'chr21'
-        exome_ht = full_exome_ht.filter(full_exome_ht.locus.contig == contig)
-    else:
-        logger.info('Filtering to autosomes and PAR regions')
-        exome_ht = full_exome_ht.filter(full_exome_ht.locus.in_autosome_or_par())
+        logger.info('Filtering to chr22 for testing')
+        context_ht = hl.filter_intervals(context_ht, [hl.parse_locus_interval(rg.contigs[21], reference_genome=rg)])
+        exome_ht = hl.filter_intervals(exome_ht, [hl.parse_locus_interval(rg.contigs[21], reference_genome=rg)])
+    #else:
+        #logger.info('Filtering to autosomes and PAR regions')
+        #context_ht = context_ht.filter(context_ht.locus.in_autosome_or_par())
 
-    logger.info('Import codon translation table and amino acid names and annotate as globals')
-    exome_ht = exome_ht.annotate_globals(codon_translation=get_codon_lookup(), acid_names=get_acid_names())
-    exome_ht.describe()
-            
+    #logger.info('Processing full context')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('This script searches for regional missense constraint in gnomAD')
 
-    pparser.add_argument('--build', help='Reference genome of dataset (37 or 38)', type=int, choices=[37, 38], default=37)
-    parser.add_argument('--overwrite', help='Overwrite everything', action='store_true')
+    parser.add_argument('--overwrite', help='Overwrite existing data', action='store_true')
     parser.add_argument('--pre_process_data', help='Pre-process data', action='store_true')
-    parser.add_argument('--test', help='Filter to chr21 (for code testing purposes)', action='store_true')
-    parser.add_argument('--trimers', help='Use trimers instead of heptamers', action='store_true')
+    parser.add_argument('--test', help='Filter to chr22 (for code testing purposes)', action='store_true')
+    parser.add_argument('--trimers', help='Use trimers instead of heptamers', action='store_false', default=True)
     parser.add_argument('--slack_channel', help='Send message to Slack channel/user', default='@kc')
     args = parser.parse_args()
 
     if args.slack_channel:
-        try_slack(args.slack_channel.split(','), main, args)
+        try_slack(args.slack_channel, main, args)
     else:
         main(args)
+    main(args)
