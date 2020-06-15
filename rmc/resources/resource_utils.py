b8287123fe02c850824b832fa287336dfe1fca5e
Katherine Chao <kchao@broadinstitute.org>
2020-06-15 14:57:51 -0400

    Added some constants to resource utils

---
+++ rmc/resources/resource_utils.py
@@ -0,0 +1,11 @@
+RESOURCE_PREFIX = "gs://regional_missense_constraint/resources"
+BUILDS = ["GRCh37", "GRCh38"]
+
+# Missense variant VEP annotations
+MISSENSE = [
+    "stop_lost",
+    "initiator_codon_variant",
+    "start_lost",
+    "protein_altering_variant",
+    "missense_variant",
+]
