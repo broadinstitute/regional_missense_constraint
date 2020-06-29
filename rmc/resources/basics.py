bdaf819baefee971643284dd1699204dc663c878
Katherine Chao <kchao@broadinstitute.org>
2020-06-29 13:56:27 -0400

    Moved lof resources to gnomad

--- rmc/resources/basics.py
+++ rmc/resources/basics.py
@@ -57,31 +57,6 @@ mutation_rate = TableResource(
 )
 
 
-## Konrad's resources
-# LoF constraint resource files
-FLAGSHIP_LOF = "gs://gnomad-public/papers/2019-flagship-lof/v1.0/"
-FLAGSHIP_MODEL_PREFIX = f"{FLAGSHIP_LOF}/model/"
-
-processed_exomes = TableResource(path=f"{FLAGSHIP_MODEL_PREFIX}/exomes_processed.ht",)
-"""
-Processed gnomAD exomes Table.
-
-Dropped colocated variants in vep annotation and removed all non-pass variants.
-Also annotated with context Table (sequence context, transcript information, most severe consequence).
-"""
-filtered_exomes = TableResource(path=f"{RESOURCE_PREFIX}/ht/exomes_missense_only.ht",)
-"""
-Processed gnomAD exomes Table filtered to missense variants only.
-"""
-processed_genomes = TableResource(path=f"{FLAGSHIP_MODEL_PREFIX}/genomes_processed.ht",)
-"""
-Processed gnomAD genomes Table.
-
-Dropped colocated variants in vep annotation and removed all non-pass variants.
-Also annotated with context Table (sequence context, transcript information, most severe consequence).
-"""
-
-
 ## Observed/expected count related resources
 # Expected variants resource files
 MODEL_PREFIX = "gs://regional_missense_constraint/model"
--- rmc/resources/grch37/gnomad.py
+++ rmc/resources/grch37/gnomad.py
@@ -1,20 +1,25 @@
 from rmc.resources.resource_utils import RESOURCE_PREFIX, FLAGSHIP_LOF
 
-## Kaitlin's resources
-# Original regional missense constraint resource files
-codon_table_path = f"{RESOURCE_PREFIX}/codons_lookup.tsv"
-acid_names_path = f"{RESOURCE_PREFIX}/acid_names.tsv"
-mutation_rate_table_path = f"{RESOURCE_PREFIX}/mutation_rate_table.tsv"
-divergence_scores_path = f"{RESOURCE_PREFIX}/divsites_gencodev19_all_transcripts.tsv"
-divergence_ht = f"{RESOURCE_PREFIX}/div_scores.ht"
-
 ## Konrad's resources
 # LoF constraint resource files
 FLAGSHIP_LOF = "gs://gnomad-public/papers/2019-flagship-lof/v1.0/"
-MODEL_PREFIX = f"{FLAGSHIP_LOF}/model/"
-processed_exomes_ht_path = f"{MODEL_PREFIX}/exomes_processed.ht"
-filtered_exomes_ht_path = f"{RESOURCE_PREFIX}/ht/exomes_missense_only.ht"
-processed_genomes_ht_path = f"{MODEL_PREFIX}/genomes_processed.ht"
+FLAGSHIP_MODEL_PREFIX = f"{FLAGSHIP_LOF}/model/"
 
-# Processed constraint resource files
-mutation_rate_ht = f"{RESOURCE_PREFIX}/ht/mutation_rate.ht"
+processed_exomes = TableResource(path=f"{FLAGSHIP_MODEL_PREFIX}/exomes_processed.ht",)
+"""
+Processed gnomAD exomes Table.
+
+Dropped colocated variants in vep annotation and removed all non-pass variants.
+Also annotated with context Table (sequence context, transcript information, most severe consequence).
+"""
+filtered_exomes = TableResource(path=f"{RESOURCE_PREFIX}/ht/exomes_missense_only.ht",)
+"""
+Processed gnomAD exomes Table filtered to missense variants only.
+"""
+processed_genomes = TableResource(path=f"{FLAGSHIP_MODEL_PREFIX}/genomes_processed.ht",)
+"""
+Processed gnomAD genomes Table.
+
+Dropped colocated variants in vep annotation and removed all non-pass variants.
+Also annotated with context Table (sequence context, transcript information, most severe consequence).
+"""
