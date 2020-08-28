bcc320d21f4cd4f3cf9f7f19a70482e34ffe1734
Katherine Chao <kchao@broadinstitute.org>
2020-08-28 15:59:03 -0400

    Updated calculate_expected with fixed calculations

--- rmc/utils/constraint.py
+++ rmc/utils/constraint.py
@@ -16,17 +16,18 @@ logger.setLevel(logging.INFO)
 
 def calculate_expected(context_ht: hl.Table, coverage_ht: hl.Table) -> hl.Table:
     """
-    Annotates context Table with expected variants count (adjusted by mutation rate, divergence score, and region type).
+    Returns table of transcripts and the total number of expected variants per transcript.
+
+    Expected variants count is adjusted by mutation rate, divergence score, and region type.
 
     .. note::
         This function is currently ExAC-specific.
         TODO: look into whether can reuse models from gnomad_lof for gnomAD v2 exomes
 
     :param Table context_ht: Context Table.
-    :return: Context Table with expected variant annotations.
+    :return: Table grouped by transcript with expected variant counts per transcript.
     :rtype: hl.Table
     """
-    logger.info("Filtering to missense only...")
     logger.info("Annotating context HT with median ExAC coverage...")
     context_ht = context_ht.annotate(exac_cov=coverage_ht[context_ht.key].median)
 
@@ -36,30 +37,40 @@ def calculate_expected(context_ht: hl.Table, coverage_ht: hl.Table) -> hl.Table:
         mu=context_ht.mu_snp * (1 + (0.31898 * context_ht.div)) * 0.9822219
     )
 
+    logger.info("Grouping by transcript...")
+    context_ht = context_ht.group_by(
+        context_ht.transcript, context_ht.region_type, context_ht.coverage
+    ).aggregate(raw_expected=hl.agg.sum(context_ht.mu))
+
     logger.info("Processing context HT to calculate number of expected variants...")
     # Annotate prediction flag based on context HT locus type
     # Region type annotation added when filtering alt and decoy contigs
-    context_ht = context_ht.annotate(
-        raw_expected=hl.case()
+    context_ht = context_ht.transmute(
+        expected=hl.case()
         .when(
-            context_ht.region_type == "x_nonpar", (0.3715167 + 7796945 * context_ht.mu)
+            context_ht.region_type == "x_nonpar",
+            (0.3715167 + 7796945 * context_ht.raw_expected),
         )
-        .when(context_ht.region_type == "y", (0.05330181 + 2457366 * context_ht.mu))
-        .default(0.4190964 + 11330208 * context_ht.mu)
+        .when(
+            context_ht.region_type == "y",
+            (0.05330181 + 2457366 * context_ht.raw_expected),
+        )
+        .default(0.4190964 + 11330208 * context_ht.raw_expected)
     )
 
     # Adjust expected counts based on depth
     context_ht = context_ht.transmute(
         expected=hl.case()
-        .when(context_ht.exac_cov < 1, 0.089 * context_ht.raw_expected)
+        .when(context_ht.coverage < 1, context_ht.expected * 0.089)
         .when(
-            (context_ht.exac_cov >= 1) & (context_ht.exac_cov < 50),
-            (0.089 + 0.217 * hl.log(context_ht.raw_expected)),
+            ((context_ht.coverage >= 1) & (context_ht.coverage < 50)),
+            (0.089 + 0.217 * hl.log(context_ht.coverage)) * context_ht.expected,
         )
-        .default(context_ht.raw_expected)
+        .default(context_ht.expected)
     )
-    context_ht = context_ht.group_by("transcript", "exon").aggregate(
-        exp=hl.agg.sum(context_ht.expected)
+
+    context_ht = context_ht.group_by("transcript").aggregate(
+        expected=hl.agg.sum(context_ht.expected)
     )
     return context_ht
 
