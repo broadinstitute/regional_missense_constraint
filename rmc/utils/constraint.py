8c11464ec89517d84cbd6c778cf1a048cb273c93
Katherine Chao <kchao@broadinstitute.org>
2020-09-15 15:39:05 -0400

    First attempt to generalize search_for_break to work for first or second break

--- rmc/utils/constraint.py
+++ rmc/utils/constraint.py
@@ -89,7 +89,7 @@ def calculate_expected(context_ht: hl.Table, coverage_ht: hl.Table) -> hl.Table:
 
 
 def get_cumulative_scan_expr(
-    transcript_expr: hl.expr.StringExpression,
+    search_expr: hl.expr.StringExpression,
     observed_expr: hl.expr.Int64Expression,
     mu_expr: hl.expr.Float64Expression,
     prediction_flag: Tuple(float, int),
@@ -103,7 +103,7 @@ def get_cumulative_scan_expr(
             - When searching for an additional break, this function should group by the section of the transcript 
                 (e.g., 'first' for before the first breakpoint or 'second' for after the first breakpoint).
 
-    :param hl.expr.StringExpression transcript_expr: Expression containing transcript if searching for first break.
+    :param hl.expr.StringExpression search_expr: Expression containing transcript if searching for first break.
         Otherwise, expression containing transcript section if searching for second additional break.
     :param hl.expr.Float64Expression mu_expr: Mutation rate expression.
     :param hl.expr.Int64Expression observed_expr: Observed variants expression.
@@ -115,12 +115,9 @@ def get_cumulative_scan_expr(
     :rtype: hl.expr.StructExpression
     """
     return hl.struct(
-        cumulative_observed=hl.scan.group_by(
-            transcript_expr, hl.scan.sum(observed_expr)
-        ),
+        cumulative_observed=hl.scan.group_by(search_expr, hl.scan.sum(observed_expr)),
         cumulative_expected=hl.scan.group_by(
-            transcript_expr,
-            prediction_flag[0] + prediction_flag[1] * hl.scan.sum(mu_expr),
+            search_expr, prediction_flag[0] + prediction_flag[1] * hl.scan.sum(mu_expr),
         ),
     )
 
@@ -264,23 +261,24 @@ def search_for_break(
     context_ht: hl.Table,
     obs_ht: hl.Table,
     exp_ht: hl.Table,
-    transcript: str,
+    search_str: str,
     prediction_flag: Tuple(float, int),
     chisq_threshold: float,
+    group_by_transcript: bool,
 ) -> Union[hl.Table, None]:
     """
     Searches for breakpoints in a transcript. 
 
-    Currently designed for one transcript at a time.
+    Currently designed to run one transcript at a time.
 
     Expects context HT to contain the following fields:
         - locus
         - alleles
-        - transcript
+        - transcript or section
         - coverage (median)
         - mu
     Also expects:
-        - multiallelic variants in context HT have been split
+        - multiallelic variants in context HT have been split.
         - context HT is autosomes/PAR only, X non-PAR only, or Y non-PAR only.
 
     Returns HT filtered to lines with maximum chisq if chisq >= max_value, otherwise returns None.
@@ -290,24 +288,30 @@ def search_for_break(
         Expects observed counts field to be named `observed`.
     :param hl.Table exp_ht: Table grouped by transcript with expected variant counts per transcript.
         Expects expected counts field to be named `expected`.
-    :param str transcript: Transcript of interest.
+    :param str search_str: String that describes search space. Either transcript when searching for first break,
+        or break section name (e.g., 'first', 'second') if searching for second additional break.
     :param Tuple(float, int) prediction_flag: Adjustments to mutation rate based on chromosomal location
         (autosomes/PAR, X non-PAR, Y non-PAR). 
         E.g., prediction flag for autosomes/PAR is (0.4190964, 11330208)
     :param float chisq_threshold: Chisq threshold for significance. 
         Value should be 10.8 (single break) and 13.8 (two breaks) (values from ExAC RMC code).
+    :param bool group_by_transcript: Whether function should group by transcript. Should be True if searching 
+        for first break and False to search for an additional break. 
     :return: Table filtered to rows with maximum chisq value IF max chisq is larger than chisq_threshold.
         Otherwise, returns None.
     :rtype: Union[hl.Table, None]
     """
-    ht = annotate_observed_expected(context_ht, obs_ht, exp_ht)
+    ht = annotate_observed_expected(context_ht, obs_ht, exp_ht, group_by_transcript)
 
     logger.info(
         "Annotating HT with cumulative expected/observed counts per transcript..."
     )
     ht = ht.annotate(
         scan_counts=get_cumulative_scan_expr(
-            ht.transcript, ht.observed, ht.mu, prediction_flag
+            search_expr=ht[search_str],
+            observed_expr=ht.observed,
+            mu_expr=ht.mu,
+            prediction_flag=prediction_flag,
         )
     )
 
@@ -317,8 +321,8 @@ def search_for_break(
         obs_exp=hl.or_missing(
             hl.len(ht.scan_counts.cumulative_observed) != 0,
             hl.min(
-                ht.scan_counts.cumulative_observed[transcript]
-                / ht.scan_counts.cumulative_expected[transcript],
+                ht.scan_counts.cumulative_observed[search_str]
+                / ht.scan_counts.cumulative_expected[search_str],
                 1,
             ),
         ),
@@ -333,8 +337,8 @@ def search_for_break(
             cond_expr=hl.len(ht.cumulative_observed) != 0,
             overall_oe_expr=ht.overall_obs_exp,
             section_oe_expr=ht.obs_exp,
-            obs_expr=ht.cumulative_observed[transcript],
-            exp_expr=ht.cumulative_expected[transcript],
+            obs_expr=ht.cumulative_observed[search_str],
+            exp_expr=ht.cumulative_expected[search_str],
         )
     )
 
@@ -345,8 +349,8 @@ def search_for_break(
             cond_expr=hl.len(ht.cumulative_observed) != 0,
             total_obs_expr=ht.total_obs,
             total_exp_expr=ht.total_exp,
-            scan_obs_expr=ht.cumulative_observed[transcript],
-            scan_exp_expr=ht.cumulative_expected[transcript],
+            scan_obs_expr=ht.cumulative_observed[search_str],
+            scan_exp_expr=ht.cumulative_expected[search_str],
         )
     )
 
@@ -365,8 +369,8 @@ def search_for_break(
             cond_expr=hl.len(ht.scan_counts.cumulative_observed) != 0,
             overall_oe_expr=ht.overall_obs_exp,
             section_oe_expr=ht.obs_exp,
-            obs_expr=ht.scan_counts.cumulative_observed[transcript],
-            exp_expr=ht.scan_counts.cumulative_expected[transcript],
+            obs_expr=ht.scan_counts.cumulative_observed[search_str],
+            exp_expr=ht.scan_counts.cumulative_expected[search_str],
         )
     )
 
