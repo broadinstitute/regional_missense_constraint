79f983d0164123be1c72b9ca3dccd6ab794ed098
Katherine Chao <kchao@broadinstitute.org>
2020-10-23 14:40:51 -0400

    Added simul_break resource to basics

--- rmc/resources/basics.py
+++ rmc/resources/basics.py
@@ -113,3 +113,15 @@ Table containing transcripts without one significant break.
 
 Input to searching for simultaneous breaks.
 """
+
+simul_break = VersionedTableResource(
+    default_version=GNOMAD_VER,
+    versions={
+        GNOMAD_VER: TableResource(
+            path=f"{CONSTRAINT_PREFIX}/{GNOMAD_VER}/simul_break.ht"
+        )
+    },
+)
+"""
+Table containing transcripts without two simultaneous breaks.
+"""
