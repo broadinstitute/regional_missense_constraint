189711f70417c0fd8ecdd2fc91e3a4e549548215
Katherine Chao <kchao@broadinstitute.org>
2020-11-17 13:24:54 -0500

    Replaced False with ~is_break

--- rmc/utils/constraint.py
+++ rmc/utils/constraint.py
@@ -703,7 +703,7 @@ def process_sections(ht: hl.Table, chisq_threshold: float):
     # Adjust is_break annotation in pre_ht
     # to prevent this function from continually finding the first significant break
     pre_ht = pre_ht.annotate(
-        is_break=hl.if_else(pre_ht.is_break_1, False, pre_ht.is_break)
+        is_break=hl.if_else(pre_ht.is_break_1, ~pre_ht.is_break_1, pre_ht.is_break)
     )
     post_ht = search_for_break(
         post_ht, search_field="transcript", chisq_threshold=chisq_threshold
