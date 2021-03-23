4d03fc0841a5c17594cb84f3b3fef5f8bf8a32a5
Katherine Chao <kchao@broadinstitute.org>
2021-03-23 11:13:55 -0400

    Updated search_for_break to fix NaN and inf chisq

--- rmc/utils/constraint.py
+++ rmc/utils/constraint.py
@@ -590,21 +590,32 @@ def search_for_break(
                 ]
             )
         else:
+            # Annotate expected variant count at current position
             ht = ht.annotate(
-                exp_at_site=ht.cumulative_exp
-                - translate_mu_to_exp_expr(
-                    ht._mu_scan, ht.transcript, ht.total_mu, ht.total_exp,
-                )
+                # Translate mu_snp at site to expected at window start site
+                # can't use translate_mu_to_exp_expr because that is expecting
+                # cumulative mu, and we want to use the value for this site only
+                exp_at_site=(ht.mu_snp / ht.total_mu)
+                * ht.total_exp,
             )
+            # Annotate observed and expected counts for section of transcript pre-window
+            ht = ht.annotate(
+                # Annotate observed count for section of transcript pre-window
+                # = current cumulative obs minus the obs at position
+                # Use hl.max to keep this value positive
+                prev_obs=hl.max(ht.cumulative_obs[ht.transcript] - ht.observed, 0),
+                prev_exp=hl.max(ht.cumulative_exp - ht.exp_at_site, 0),
+            )
+            # Make sure prev exp value isn't 0
+            # Otherwise, the chisq calculation will return NaN
+            ht = ht.annotate(prev_exp=hl.if_else(ht.prev_exp > 0, ht.prev_exp, 1e-09))
+            # Annotate OE value for section of transcript pre-window
             ht = ht.annotate(
                 pre_oe=get_obs_exp_expr(
-                    cond_expr=True,
-                    obs_expr=ht.cumulative_obs[ht.transcript] - ht.observed,
-                    exp_expr=ht.exp_at_site,
+                    cond_expr=True, obs_expr=ht.prev_obs, exp_expr=ht.prev_exp,
                 )
             )
-            # Fix previous section OE if NaN (otherwise chisq calculations won't work)
-            ht = ht.annotate(pre_oe=hl.if_else(~hl.is_nan(ht.pre_oe), ht.pre_oe, 1e-09))
+
             ht = ht.annotate(
                 section_nulls=[
                     # Get null expression for section of transcript pre-window
@@ -612,8 +623,8 @@ def search_for_break(
                     get_dpois_expr(
                         cond_expr=True,
                         section_oe_expr=ht.overall_oe,
-                        obs_expr=ht.cumulative_obs[ht.transcript] - ht.observed,
-                        exp_expr=ht.exp_at_site,
+                        obs_expr=ht.prev_obs,
+                        exp_expr=ht.prev_exp,
                     ),
                     # Get null expression for window of constraint
                     # These values are the cumulative values at the first position post-window minus
@@ -650,8 +661,8 @@ def search_for_break(
                     get_dpois_expr(
                         cond_expr=True,
                         section_oe_expr=ht.pre_oe,
-                        obs_expr=ht.cumulative_obs[ht.transcript] - ht.observed,
-                        exp_expr=ht.exp_at_site,
+                        obs_expr=ht.prev_obs,
+                        exp_expr=ht.prev_exp,
                     ),
                     # Get alt expression for window of constraint
                     get_dpois_expr(
