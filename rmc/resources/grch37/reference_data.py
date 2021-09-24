663d034f1c9782c050df8933484123ad6d3c3fd6
Katherine Chao <kchao@broadinstitute.org>
2021-09-24 10:29:15 -0400

    Added dnm resources

--- rmc/resources/grch37/reference_data.py
+++ rmc/resources/grch37/reference_data.py
@@ -26,3 +26,22 @@ processed_context = VersionedTableResource(
         )
     },
 )
+
+
+## Assessment related resources
+de_novo_tsv = f"{RESOURCE_PREFIX}/GRCh37/reference_data/fordist_KES_combined_asc_dd_dnms_2020_04_21_annotated.txt"
+"""
+De novo missense variants from 31,058 cases with developmental disorders, 6,430 cases with autism spectrum disorders, and 2,179 controls.
+
+Controls are the siblings of the autism cases.
+Samples are from:
+Kaplanis et al. (Evidence for 28 genetic disorders discovered by combining healthcare and research data.)
+Satterstrom et al. (Large-Scale Exome Sequencing Study Implicates Both Developmental and Functional Changes in the Neurobiology of Autism.)
+"""
+
+de_novo = TableResource(
+    path=f"{RESOURCE_PREFIX}/GRCh37/reference_data/ht/dd_de_novo.ht",
+)
+"""
+De novo missense variants from 37,488 cases and 2,179 controls (same as above).
+"""
