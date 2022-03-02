276321f4ba636e36a9d862b2a45e5c9bdd537d2c
Katherine Chao <kchao@broadinstitute.org>
2022-03-02 13:29:32 -0500

    Reformatted b37 resources

--- rmc/resources/grch37/reference_data.py
+++ rmc/resources/grch37/reference_data.py
@@ -26,3 +26,33 @@ processed_context = VersionedTableResource(
         )
     },
 )
+
+gene_model = TableResource(path=f"{RESOURCE_PREFIX}/GRCh37/browser/b37_transcripts.ht")
+"""
+Table containing transcript start and stop positions displayed in the browser.
+"""
+
+
+## Assessment related resources
+clinvar_path_mis = TableResource(
+    path=f"{RESOURCE_PREFIX}/GRCh37/reference_data/ht/clinvar_pathogenic_missense.ht",
+)
+"""
+ClinVar pathogenic/likely pathogenic missense variants in haploinsufficient genes that cause severe disease.
+"""
+
+de_novo_tsv = f"{RESOURCE_PREFIX}/GRCh37/reference_data/fordist_KES_combined_asc_dd_dnms_2020_04_21_annotated.txt"
+"""
+De novo missense variants from 31,058 cases with developmental disorders, 6,430 cases with autism spectrum disorders, and 2,179 controls.
+Controls are the siblings of the autism cases.
+Samples are from:
+Kaplanis et al. (Evidence for 28 genetic disorders discovered by combining healthcare and research data.)
+Satterstrom et al. (Large-Scale Exome Sequencing Study Implicates Both Developmental and Functional Changes in the Neurobiology of Autism.)
+"""
+
+de_novo = TableResource(
+    path=f"{RESOURCE_PREFIX}/GRCh37/reference_data/ht/dd_de_novo.ht",
+)
+"""
+De novo missense variants from 37,488 cases and 2,179 controls (same as above).
+"""
