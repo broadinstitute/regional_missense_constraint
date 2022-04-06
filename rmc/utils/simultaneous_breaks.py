cec346d0115b5ee1beaa7a91f448789e90b1546c
Katherine Chao <kchao@broadinstitute.org>
2022-04-06 10:53:39 -0400

    Update rmc/utils/simultaneous_breaks.py
    Co-authored-by: lw453 <30123882+lw453@users.noreply.github.com>

--- rmc/utils/simultaneous_breaks.py
+++ rmc/utils/simultaneous_breaks.py
@@ -603,7 +603,7 @@ def process_transcript_group(
     :param float chisq_threshold: Chi-square significance threshold. Default is 9.2.
         This value corresponds to a p-value of 0.01 with 2 degrees of freedom.
         (https://www.itl.nist.gov/div898/handbook/eda/section3/eda3674.htm)
-        Default value used in ExAC was 13.8, which corresponds to a p-value of 0.999.
+        Default value used in ExAC was 13.8, which corresponds to a p-value of 0.001.
     :param int split_window_size: Window size to search for transcripts that have more
         possible missense variants than threshold. Only used if over_threshold is True.
     :param bool read_if_exists: Whether to read temporary Table if it already exists rather than overwrite.
