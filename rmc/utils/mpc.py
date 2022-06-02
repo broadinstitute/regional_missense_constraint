72e53cb827b4d8463e8c33c625cb617536ad0ed0
Katherine Chao <kchao@broadinstitute.org>
2022-06-02 17:48:23 -0400

    Update rmc/utils/mpc.py
    Co-authored-by: lw453 <30123882+lw453@users.noreply.github.com>

--- rmc/utils/mpc.py
+++ rmc/utils/mpc.py
@@ -176,7 +176,7 @@ def get_annotations_from_context_ht_vep(
 
     :param hl.Table ht: Input VEP context Table.
     :param List[str] annotations: Additional annotations to extract from context HT's VEP field.
-    :return: VEP context Table with non-coding loci removed/filtered to informative amino acids
+    :return: VEP context Table with rows filtered to remove loci that are non-coding and retain only those with informative amino acids
         and filtered to keep only specified annotations.
     """
     annot_expr = {
