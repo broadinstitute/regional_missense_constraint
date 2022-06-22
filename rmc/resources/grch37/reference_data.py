10369e37df2bd68c26654f909f131f291bc5d3e9
Katherine Chao <kchao@broadinstitute.org>
2022-06-22 15:01:09 -0400

    Update rmc/resources/grch37/reference_data.py
    Co-authored-by: lw453 <30123882+lw453@users.noreply.github.com>

--- rmc/resources/grch37/reference_data.py
+++ rmc/resources/grch37/reference_data.py
@@ -22,7 +22,7 @@ Used to calculate the cumulative observed and expected missense values per locus
 generate regional missense constraint results.
 
 NOTE: This resource is created with `process_vep`, which now filters to non-outlier transcripts by default.
-However, for v2, this resource contains *all* transcripts (including outliers).
+However, for v2, this resource contains *all* canonical transcripts (including outliers).
 """
 
 gene_model = TableResource(path=f"{RESOURCE_PREFIX}/GRCh37/browser/b37_transcripts.ht")
