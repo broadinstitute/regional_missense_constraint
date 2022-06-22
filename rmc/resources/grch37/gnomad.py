dd9f7387fe9579589ed2dd9466d82a6199aa8682
Katherine Chao <kchao@broadinstitute.org>
2022-06-22 13:47:56 -0400

    Updated filtered_exomes description

--- rmc/resources/grch37/gnomad.py
+++ rmc/resources/grch37/gnomad.py
@@ -18,7 +18,7 @@ filtered_exomes = TableResource(
     path=f"{RESOURCE_PREFIX}/GRCh37/gnomad/ht/exomes_missense_only.ht"
 )
 """
-Processed gnomAD exomes Table filtered to missense variants only.
+Processed gnomAD exomes Table filtered to rare (AF < 0.001) missense variants in canonical transcripts only.
 
 NOTE: This resource is created with `process_vep`, which now filters to non-outlier transcripts by default.
 However, for v2, this resource contains *all* transcripts (including outliers).
