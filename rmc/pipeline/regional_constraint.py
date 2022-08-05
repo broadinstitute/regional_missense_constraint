d8245020f5b7cf2ab6735977fc33d4d5fc996145
Katherine Chao <kchao@broadinstitute.org>
2022-08-05 10:03:03 -0400

    Temporary reconfiguration of pipeline code to remove any references to gnomad_lof

--- rmc/pipeline/regional_constraint.py
+++ rmc/pipeline/regional_constraint.py
@@ -8,44 +8,17 @@ from gnomad.utils.file_utils import file_exists
 from gnomad.utils.slack import slack_notifications
 
 from rmc.resources.basics import (
-    constraint_prep,
     LOGGING_PATH,
     multiple_breaks,
-    not_one_break,
     one_break,
     simul_break,
     temp_path,
 )
-from rmc.resources.grch37.exac import filtered_exac
-from rmc.resources.grch37.gnomad import (
-    constraint_ht,
-    filtered_exomes,
-    processed_exomes,
-    prop_obs_coverage,
-)
+from rmc.resources.grch37.gnomad import constraint_ht
 from rmc.resources.grch37.reference_data import filtered_context
-from rmc.resources.resource_utils import CURRENT_VERSION, MISSENSE
+from rmc.resources.resource_utils import CURRENT_VERSION
 from rmc.slack_creds import slack_token
-from rmc.utils.constraint import (
-    add_obs_annotation,
-    calculate_exp_per_transcript,
-    calculate_observed,
-    fix_xg,
-    get_fwd_exprs,
-    GROUPINGS,
-    process_additional_breaks,
-    process_transcripts,
-)
-from rmc.utils.generic import (
-    filter_context_using_gnomad,
-    filter_to_region_type,
-    generate_models,
-    get_constraint_transcripts,
-    get_coverage_correction_expr,
-    keep_criteria,
-    process_context_ht,
-    process_vep,
-)
+from rmc.utils.constraint import process_additional_breaks
 
 
 logging.basicConfig(
@@ -56,242 +29,51 @@ logger = logging.getLogger("regional_missense_constraint")
 logger.setLevel(logging.INFO)
 
 
+def get_constraint_transcripts(outlier: bool = True) -> hl.expr.SetExpression:
+    """
+    Read in LoF constraint HT results to get set of transcripts.
+
+    Return either set of transcripts to keep (transcripts that passed transcript QC)
+    or outlier transcripts.
+
+    Transcripts are removed for the reasons detailed here:
+    https://gnomad.broadinstitute.org/faq#why-are-constraint-metrics-missing-for-this-gene-or-annotated-with-a-note
+
+    :param bool outlier: Whether to filter LoF constraint HT to outlier transcripts (if True),
+        or QC-pass transcripts (if False). Default is True.
+    :return: Set of outlier transcripts or transcript QC pass transcripts.
+    :rtype: hl.expr.SetExpression
+    """
+    logger.warning(
+        "Assumes LoF constraint has been separately calculated and that constraint HT exists..."
+    )
+    if not file_exists(constraint_ht.path):
+        raise DataException("Constraint HT not found!")
+
+    constraint_transcript_ht = constraint_ht.ht().key_by("transcript")
+    constraint_transcript_ht = constraint_transcript_ht.filter(
+        constraint_transcript_ht.canonical
+    ).select("constraint_flag")
+    if outlier:
+        constraint_transcript_ht = constraint_transcript_ht.filter(
+            hl.len(constraint_transcript_ht.constraint_flag) > 0
+        )
+    else:
+        constraint_transcript_ht = constraint_transcript_ht.filter(
+            hl.len(constraint_transcript_ht.constraint_flag) == 0
+        )
+    return hl.literal(
+        constraint_transcript_ht.aggregate(
+            hl.agg.collect_as_set(constraint_transcript_ht.transcript)
+        )
+    )
+
+
 def main(args):
     """Call functions from `constraint.py` to calculate regional missense constraint."""
     exac = args.exac
 
     try:
-        if args.pre_process_data:
-            hl.init(log="/RMC_pre_process.log")
-            # TODO: Add code to create annotations necessary for constraint_flag_expr and filter transcripts prior to running constraint
-            logger.warning("Code currently only processes b37 data!")
-
-            logger.info("Preprocessing reference fasta (context) HT...")
-            context_ht = process_context_ht("GRCh37", args.trimers)
-
-            logger.info(
-                "Filtering context HT to all covered sites not found or rare in gnomAD exomes"
-            )
-            context_ht = filter_context_using_gnomad(
-                context_ht, "exomes", filter_context_using_cov=True
-            )
-            context_ht.write(filtered_context.path, overwrite=args.overwrite)
-
-            logger.info(
-                "Filtering gnomAD exomes HT to missense variants in canonical transcripts only..."
-            )
-            exome_ht = processed_exomes.ht()
-            exome_ht = process_vep(exome_ht, filter_csq=True, csq=MISSENSE)
-
-            # Move nested annotations into top level annotations
-            exome_ht = exome_ht.select(
-                ac=exome_ht.freq[0].AC,
-                af=exome_ht.freq[0].AF,
-                pass_filters=exome_ht.pass_filters,
-                exome_coverage=exome_ht.coverage.exomes.median,
-                transcript_consequences=exome_ht.transcript_consequences,
-            )
-            exome_ht = exome_ht.filter(keep_criteria(exome_ht))
-            exome_ht.write(filtered_exomes.path, overwrite=args.overwrite)
-
-            logger.info("Done preprocessing files")
-
-        if args.prep_for_constraint:
-            hl.init(log="/RMC_prep_for_constraint.log")
-            logger.info("Reading in exome HT...")
-            if exac:
-                exome_ht = filtered_exac.ht()
-
-            else:
-                exome_ht = filtered_exomes.ht()
-
-            logger.info("Reading in context HT...")
-            context_ht = filtered_context.ht()
-
-            logger.info("Building plateau and coverage models...")
-            coverage_ht = prop_obs_coverage.ht()
-            coverage_x_ht = hl.read_table(
-                prop_obs_coverage.path.replace(".ht", "_x.ht")
-            )
-            coverage_y_ht = hl.read_table(
-                prop_obs_coverage.path.replace(".ht", "_y.ht")
-            )
-
-            (
-                coverage_model,
-                plateau_models,
-                plateau_x_models,
-                plateau_y_models,
-            ) = generate_models(
-                coverage_ht, coverage_x_ht, coverage_y_ht, trimers=args.trimers
-            )
-
-            context_ht = context_ht.annotate_globals(
-                plateau_models=plateau_models,
-                plateau_x_models=plateau_x_models,
-                plateau_y_models=plateau_y_models,
-                coverage_model=coverage_model,
-            )
-            context_ht = context_ht.annotate(
-                coverage_correction=get_coverage_correction_expr(
-                    context_ht.exome_coverage,
-                    coverage_model,
-                    args.high_cov_cutoff,
-                )
-            )
-
-            if not args.skip_calc_oe:
-                logger.info(
-                    "Adding coverage correction to mutation rate probabilities..."
-                )
-                context_ht = context_ht.annotate(
-                    raw_mu_snp=context_ht.mu_snp,
-                    mu_snp=context_ht.mu_snp
-                    * get_coverage_correction_expr(
-                        context_ht.exome_coverage, context_ht.coverage_model
-                    ),
-                )
-
-                logger.info(
-                    "Creating autosomes-only, chrX non-PAR-only, and chrY non-PAR-only HT versions..."
-                )
-                context_x_ht = filter_to_region_type(context_ht, "chrX")
-                context_y_ht = filter_to_region_type(context_ht, "chrY")
-                context_auto_ht = filter_to_region_type(context_ht, "autosomes")
-
-                logger.info("Calculating expected values per transcript...")
-                exp_ht = calculate_exp_per_transcript(
-                    context_auto_ht,
-                    locus_type="autosomes",
-                    groupings=GROUPINGS,
-                )
-                exp_x_ht = calculate_exp_per_transcript(
-                    context_x_ht,
-                    locus_type="X",
-                    groupings=GROUPINGS,
-                )
-                exp_y_ht = calculate_exp_per_transcript(
-                    context_y_ht,
-                    locus_type="Y",
-                    groupings=GROUPINGS,
-                )
-                exp_ht = exp_ht.union(exp_x_ht).union(exp_y_ht)
-
-                logger.info(
-                    "Fixing expected values for genes that span PAR and nonPAR regions..."
-                )
-                # Adding a sum here to make sure that genes like XG that span PAR/nonPAR regions
-                # have correct total expected values
-                exp_ht = exp_ht.group_by(transcript=exp_ht.transcript).aggregate(
-                    total_exp=hl.agg.sum(exp_ht.expected),
-                    total_mu=hl.agg.sum(exp_ht.mu_agg),
-                )
-
-                logger.info(
-                    "Aggregating total observed variant counts per transcript..."
-                )
-                obs_ht = calculate_observed(exome_ht)
-
-            else:
-                logger.warning(
-                    "Using observed and expected values calculated on gnomAD v2.1.1 exomes..."
-                )
-                exp_ht = (
-                    constraint_ht.ht()
-                    .key_by("transcript")
-                    .select("obs_mis", "exp_mis", "oe_mis")
-                )
-
-                # Filter to canonical transcripts only and rename fields
-                exp_ht = exp_ht.filter(exp_ht.canonical)
-                exp_ht = exp_ht.transmute(
-                    observed=exp_ht.obs_mis,
-                    expected=exp_ht.exp_mis,
-                    mu_agg=exp_ht.mu_mis,
-                )
-                obs_ht = exp_ht
-
-            logger.info(
-                "Annotating context HT with number of observed and expected variants per site..."
-            )
-            # Add observed variants to context HT
-            context_ht = add_obs_annotation(context_ht, filter_csq=True)
-
-            logger.info(
-                "Collecting by key to run constraint per base and not per base-allele..."
-            )
-            # Context HT is keyed by locus and allele, which means there is one row for every possible missense variant
-            # This means that any locus could be present up to three times (once for each possible missense)
-            # Collect by key here to ensure all loci are unique
-            context_ht = context_ht.key_by("locus", "transcript").collect_by_key()
-            context_ht = context_ht.annotate(
-                # Collect the mutation rate probabilities at each locus
-                mu_snp=hl.sum(context_ht.values.mu_snp),
-                # Collect the observed counts for each locus
-                # (this includes counts for each possible missense at the locus)
-                observed=hl.sum(context_ht.values.observed),
-                # Take just the first coverage value, since the locus should have the same coverage across the possible variants
-                coverage=context_ht.values.exome_coverage[0],
-            )
-
-            logger.info(
-                "Annotating total observed and expected values and overall observed/expected value "
-                "(capped at 1) per transcript..."
-            )
-            context_ht = context_ht.annotate(
-                total_exp=exp_ht[context_ht.transcript].total_exp,
-                total_mu=exp_ht[context_ht.transcript].total_mu,
-                total_obs=obs_ht[context_ht.transcript].observed,
-            )
-            context_ht = context_ht.annotate(
-                overall_oe=hl.min(context_ht.total_obs / context_ht.total_exp, 1)
-            )
-
-            context_ht = get_fwd_exprs(
-                ht=context_ht,
-                transcript_str="transcript",
-                obs_str="observed",
-                mu_str="mu_snp",
-                total_mu_str="total_mu",
-                total_exp_str="total_exp",
-            )
-
-            context_ht = context_ht.write(
-                constraint_prep.path, overwrite=args.overwrite
-            )
-
-        if args.search_for_first_break:
-            hl.init(log="/RMC_first_break.log")
-
-            logger.info("Searching for transcripts with a significant break...")
-            context_ht = constraint_prep.ht()
-            context_ht = process_transcripts(context_ht, args.chisq_threshold)
-            context_ht = context_ht.checkpoint(
-                f"{temp_path}/first_break.ht", overwrite=True
-            )
-
-            logger.info(
-                "Filtering HT to transcripts with one significant break and writing..."
-            )
-            is_break_ht = context_ht.filter(context_ht.is_break)
-            transcripts = is_break_ht.aggregate(
-                hl.agg.collect_as_set(is_break_ht.transcript), _localize=False
-            )
-            one_break_ht = context_ht.filter(
-                transcripts.contains(context_ht.transcript)
-            )
-            one_break_ht = one_break_ht.annotate_globals(
-                break_1_transcripts=transcripts
-            )
-            one_break_ht.write(one_break.path, overwrite=args.overwrite)
-
-            logger.info(
-                "Filtering HT to transcripts without a significant break and writing..."
-            )
-            not_one_break_ht = context_ht.anti_join(one_break_ht)
-            not_one_break_ht = not_one_break_ht.drop("values")
-            not_one_break_ht.write(not_one_break.path, overwrite=args.overwrite)
-
         if args.search_for_additional_breaks:
             hl.init(log="/RMC_additional_breaks.log")
 
@@ -305,7 +87,9 @@ def main(args):
 
             # Remove outlier transcripts
             outlier_transcripts = get_constraint_transcripts(outlier=True)
-            context_ht = context_ht.filter(~outlier_transcripts.contains(context_ht.transcript))
+            context_ht = context_ht.filter(
+                ~outlier_transcripts.contains(context_ht.transcript)
+            )
 
             # Add break_list annotation to context HT
             context_ht = context_ht.annotate(break_list=[context_ht.is_break])
@@ -359,65 +143,8 @@ def main(args):
                 break_ht = break_ht.filter(transcripts.contains(break_ht.transcript))
                 break_num += 1
 
-            #context_ht.write(multiple_breaks.path, overwrite=args.overwrite)
+            # context_ht.write(multiple_breaks.path, overwrite=args.overwrite)
             context_ht.write(f"{temp_path}/multiple_breaks.ht", overwrite=True)
-
-        # NOTE: This is only necessary for gnomAD v2
-        # Fixed expected counts for any genes that span PAR and non-PAR regions
-        # after running on gnomAD v2
-        if args.fix_xg:
-            hl.init(log="/RMC_fix_XG.log")
-
-            logger.info("Reading in exome HT...")
-            exome_ht = filtered_exomes.ht()
-
-            logger.info("Reading in context HT...")
-            context_ht = filtered_context.ht()
-
-            logger.info("Adding models from constraint prep HT...")
-            constraint_prep_ht = constraint_prep.ht().select()
-            context_ht = context_ht.annotate_globals(
-                **constraint_prep_ht.index_globals()
-            )
-
-            logger.info("Adding coverage correction to mutation rate probabilities...")
-            context_ht = context_ht.annotate(
-                raw_mu_snp=context_ht.mu_snp,
-                mu_snp=context_ht.mu_snp
-                * get_coverage_correction_expr(
-                    context_ht.exome_coverage, context_ht.coverage_model
-                ),
-            )
-
-            logger.info(
-                "Fixing XG (gene that spans PAR and non-PAR regions on chrX)..."
-            )
-            xg = fix_xg(context_ht, exome_ht, args.xg_transcript)
-
-            logger.info("Searching for a break in XG...")
-            xg = process_transcripts(xg, chisq_threshold=args.chisq_threshold)
-
-            logger.info("Checking whether there was one break...")
-            is_break_ht = xg.filter(xg.is_break)
-            if is_break_ht.count() == 0:
-                logger.info("XG didn't have one single significant break...")
-                transcript_ht = xg.group_by(xg.transcript).aggregate(
-                    end_pos=hl.agg.max(xg.locus.position),
-                    start_pos=hl.agg.min(xg.locus.position),
-                )
-                xg = xg.annotate(
-                    start_pos=transcript_ht[xg.transcript].start_pos,
-                    end_pos=transcript_ht[xg.transcript].end_pos,
-                )
-            else:
-                logger.info("XG has at least one break!")
-                is_break_ht = is_break_ht.checkpoint(
-                    f"{temp_path}/XG_one_break.ht", overwrite=args.overwrite
-                )
-                # NOTE: Did not need to check for additional breaks in XG
-                # XG did not have a single significant break in gnomAD v2
-                # xg = xg.annotate(is_break=is_break_ht[ht.key].is_break)
-                # xg = xg.annotate(break_list=[xg.is_break])
 
         if args.finalize:
             hl.init(log="/RMC_finalize.log")
@@ -606,58 +333,15 @@ if __name__ == "__main__":
         "--trimers", help="Use trimers instead of heptamers", action="store_true"
     )
     parser.add_argument(
-        "--exac", help="Use ExAC Table (not gnomAD Table)", action="store_true"
-    )
-    parser.add_argument(
-        "--n-partitions",
-        help="Desired number of partitions for output data",
-        type=int,
-        default=40000,
-    )
-    parser.add_argument(
-        "--high-cov-cutoff",
-        help="Coverage threshold for a site to be considered high coverage",
-        type=int,
-        default=40,
-    )
-    parser.add_argument(
         "--chisq-threshold",
         help="Chi-square significance threshold. Value should be 10.8 (single break) and 13.8 (two breaks) (values from ExAC RMC code).",
         type=float,
         default=10.8,
     )
     parser.add_argument(
-        "--pre-process-data", help="Pre-process data", action="store_true"
-    )
-    parser.add_argument(
-        "--prep-for-constraint",
-        help="Prepare tables for constraint calculations",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--skip-calc-oe",
-        help="Skip observed and expected variant calculations per transcript. Relevant only to gnomAD v2.1.1!",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--search-for-first-break",
-        help="Initial search for one break in all transcripts",
-        action="store_true",
-    )
-    parser.add_argument(
         "--search-for-additional-breaks",
         help="Search for additional break in transcripts with one significant break",
         action="store_true",
-    )
-    parser.add_argument(
-        "--fix-xg",
-        help="Fix XG (gene that spans PAR and non-PAR regions on chrX). Required only for gnomAD v2",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--xg-transcript",
-        help="Transcript ID for XG",
-        default="ENST00000419513",
     )
     parser.add_argument(
         "--finalize",
