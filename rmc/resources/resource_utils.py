c1e241951f4aad8e2fbebeedf07e39dbca4f89bc
Katherine Chao <kchao@broadinstitute.org>
2022-08-08 10:24:22 -0400

    Removed bucket resources from resource_utils

--- rmc/resources/resource_utils.py
+++ rmc/resources/resource_utils.py
@@ -1,7 +1,4 @@
 BUILDS = ["GRCh37", "GRCh38"]
-FLAGSHIP_LOF = "gs://gnomad-public-requester-pays/papers/2019-flagship-lof/v1.0"
-RMC_PREFIX = "gs://regional_missense_constraint"
-RESOURCE_PREFIX = "gs://regional_missense_constraint/resources"
 CURRENT_VERSION = "2.1.1"
 GNOMAD_VERSIONS = ["2.1.1"]
 
