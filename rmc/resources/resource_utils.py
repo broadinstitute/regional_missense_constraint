aab0db9c3f4360ea8c41894aad2e4845e65d32a9
lw453 <30123882+lw453@users.noreply.github.com>
2022-09-13 11:36:35 -0400

    Add back lists for builds, gnomAD versions

--- rmc/resources/resource_utils.py
+++ rmc/resources/resource_utils.py
@@ -1,6 +1,9 @@
 """Script containing resource utility constants."""
 CURRENT_BUILD = "GRCh37"
+BUILDS = ["GRCh37"]
+
 CURRENT_GNOMAD_VERSION = "2.1.1"
+GNOMAD_VERSIONS = ["2.1.1"]
 
 MISSENSE = "missense_variant"
 """
