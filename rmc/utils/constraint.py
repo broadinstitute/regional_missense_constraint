46c93b96453fa3077012c7d4c3efe8ee9ab6ceca
Katherine Chao <kchao@broadinstitute.org>
2022-12-06 15:18:22 -0500

    remove quotes from temp code

--- rmc/utils/constraint.py
+++ rmc/utils/constraint.py
@@ -833,7 +833,7 @@ def get_rescue_1break_transcripts(
 
     # Read in the no break found HT from single search round 1
     # (of initial search)
-    """ht = hl.read_table(
+    ht = hl.read_table(
         single_search_round_ht_path(
             is_rescue=False,
             search_num=1,
@@ -879,7 +879,7 @@ def get_rescue_1break_transcripts(
             is_breakpoint_only=False,
         ),
         overwrite=overwrite,
-    )"""
+    )
     ht = hl.read_table(
         single_search_round_ht_path(
             is_rescue=True,
@@ -929,13 +929,13 @@ def get_rescue_2breaks_transcripts(
 
     # Merge all of the temporary chi square HTs saved in round 1 of
     # initial simul breaks search
-    """merge_simul_break_temp_hts(
+    merge_simul_break_temp_hts(
         input_hts_path=SIMUL_BREAK_TEMP_PATH,
         batch_phrase="batch_temp_chisq",
         query_phrase="dataproc_temp_chisq",
         output_ht_path=f"{TEMP_PATH_WITH_DEL}/rescue_simul_chisq.ht",
         overwrite=overwrite,
-    )"""
+    )
 
     ht = hl.read_table(f"{TEMP_PATH_WITH_DEL}/rescue_simul_chisq.ht")
     ht = get_max_chisq_per_group(ht, "section", "max_chisq", True)
