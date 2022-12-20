0713bf6bd81c0d0282d5466266181a94ede2fe59
lw453 <30123882+lw453@users.noreply.github.com>
2022-12-20 09:48:06 -0500

    Fix outlier removal in `process_vep`

--- rmc/utils/generic.py
+++ rmc/utils/generic.py
@@ -322,7 +322,7 @@ def process_vep(ht: hl.Table, filter_csq: bool = False, csq: str = None) -> hl.T
 
     logger.info("Filtering to non-outlier transcripts...")
     # Keep transcripts used in LoF constraint only (remove all other outlier transcripts)
-    constraint_transcripts = get_constraint_transcripts(outlier=True)
+    constraint_transcripts = get_constraint_transcripts(outlier=False)
     ht = ht.filter(
         constraint_transcripts.contains(ht.transcript_consequences.transcript_id)
     )
