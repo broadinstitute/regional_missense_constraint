a3e8a6376fc26793d1501a160bdba67a24bc005c
ch-kr <kchao@broadinstitute.org>
2023-05-03 17:07:16 -0400

    update adj freq index description

--- rmc/utils/generic.py
+++ rmc/utils/generic.py
@@ -228,8 +228,8 @@ def get_gnomad_public_release(
     :param gnomad_data_type: gnomAD data type. Used to retrieve public release and coverage resources.
         Must be one of "exomes" or "genomes" (check is done within `public_release`).
         Default is "exomes".
-    :param adj_freq_index: Index of frequency array that contains global population calculated on
-        high quality (adj) genotypes. Default is 0.
+    :param adj_freq_index: Index of frequency array that contains frequency information calculated on
+        high quality (adj) genotypes across all genetic ancestry groups. Default is 0.
     :return: gnomAD public sites HT annotated with coverage and filtered to select fields.
     """
     gnomad = public_release(gnomad_data_type).ht().select_globals()
@@ -255,8 +255,8 @@ def filter_context_using_gnomad(
     :param gnomad_data_type: gnomAD data type. Used to retrieve public release and coverage resources.
         Must be one of "exomes" or "genomes" (check is done within `public_release`).
         Default is "exomes".
-    :param adj_freq_index: Index of frequency array that contains global population calculated on
-        high quality (adj) genotypes. Default is 0.
+    :param adj_freq_index: Index of frequency array that contains frequency information calculated on
+        high quality (adj) genotypes across all genetic ancestry groups. Default is 0.
     :param cov_threshold: Remove variants below this median coverage threshold. Default is 0.
     :return: Filtered VEP context Table.
     """
