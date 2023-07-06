d25531d26c57d06e159f1d24215309a6e8190e27
lw453 <30123882+lw453@users.noreply.github.com>
2023-07-06 12:48:48 -0400

    Adjust overwrite documentation and usage

--- rmc/pipeline/regional_constraint.py
+++ rmc/pipeline/regional_constraint.py
@@ -562,7 +562,9 @@ def main(args):
             rmc_ht = rmc_ht.annotate_globals(p_value=p_value)
 
             logger.info("Writing out RMC results...")
-            rmc_ht.write(rmc_results.versions[args.freeze].path)
+            rmc_ht.write(
+                rmc_results.versions[args.freeze].path, overwrite=args.overwrite
+            )
 
             logger.info("Getting transcripts without evidence of RMC...")
             create_no_breaks_he(freeze=args.freeze, overwrite=args.overwrite)
@@ -663,7 +665,12 @@ if __name__ == "__main__":
         default=CURRENT_FREEZE,
     )
     parser.add_argument(
-        "--overwrite", help="Overwrite existing data.", action="store_true"
+        "--overwrite",
+        help="""
+        Overwrite existing output data.
+        Applies to all outputs except OE-annotated context table (created in `--finalize`).
+        """,
+        action="store_true",
     )
     parser.add_argument(
         "--overwrite-temp",
