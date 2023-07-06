8a02915665512e9d7906b962137180d066691066
lw453 <30123882+lw453@users.noreply.github.com>
2023-07-06 09:59:46 -0400

    Declutter notes in documentation

--- rmc/resources/reference_data.py
+++ rmc/resources/reference_data.py
@@ -76,18 +76,23 @@ def train_val_test_transcripts_path(
 
     By default, all training transcripts are returned.
 
+    .. note::
+        - `is_test` cannot be True if `fold` is defined.
+        - `is_test` and `is_val` cannot be True simultaneously.
+        - If `is_val` is True, `fold` must also be defined.
+        - If specified, `fold` value must be between 1 and `FOLD_K`.
+
     :param is_test: Whether to return test transcripts.
         If False, training transcripts will be returned. If True, test transcripts will be returned.
         Default is False.
     :param fold: Fold number in training set to select transcripts from.
         If None, all training transcripts will be returned.
         If not None, only validation or training transcripts from the specified fold will be returned.
-        Default is None. NOTE that `is_test` must be False if `fold` is not None.
+        Default is None.
     :param is_val: Whether to return validation transcripts.
         If False, training transcripts from the specified fold of the training set will be returned.
         If True, validation transcripts from the specified fold of the training set will be returned.
-        Default is False. NOTE that `is_test` and `is_val` cannot both be true,
-        and that `fold` must not be None if `is_val` is True.
+        Default is False.
     :return: Path to Table.
     """
     if is_test and is_val:
