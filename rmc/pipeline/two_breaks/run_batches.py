cc0790c95394177d9c7588f0f97aef46d3baa0f8
ch-kr <kchao@broadinstitute.org>
2024-07-19 13:23:19 -0400

    revert changes to run batches but still remove custom machine, also point docker to Hail docker

--- rmc/pipeline/two_breaks/run_batches.py
+++ rmc/pipeline/two_breaks/run_batches.py
@@ -1,15 +1,24 @@
 """
 This script searches for two simultaneous breaks in groups of transcripts using Hail Batch.
 
-# TODO: Add RMC repo to docker image and make sure it's up to date
-NOTE: `process_section_group` has been copied here to make sure Hail initializes properly
-(and can read from requester-pays buckets) in Batch.
+Note that a couple functions have been copied into this script from `constraint.py`:
+- `get_obs_exp_expr`
+- `get_dpois_expr`
+
+Note also that a few functions have been copied into this script from `simultaneous_breaks.py`:
+- `calculate_window_chisq`
+- `search_for_two_breaks`
+- `process_section_group`
+
+This is because python imports do not work in Hail Batch PythonJobs unless
+the python scripts are included within the provided Dockerfile, and this
+script currently relies on Hail's docker image.
 
 This script should be run locally because it submits jobs to the Hail Batch service.
 """
 import argparse
 import logging
-from typing import List
+from typing import List, Optional, Set
 
 import hail as hl
 import hailtop.batch as hb
@@ -17,17 +26,16 @@ import scipy
 from gnomad.utils.slack import slack_notifications
 from tqdm import tqdm
 
-from rmc.resources.basics import RMC_PREFIX, TEMP_PATH_WITH_FAST_DEL
+from rmc.resources.basics import TEMP_PATH_WITH_FAST_DEL
 from rmc.resources.rmc import (
     CURRENT_FREEZE,
+    MIN_CHISQ_THRESHOLD,
     MIN_EXP_MIS,
     P_VALUE,
     grouped_single_no_break_ht_path,
-    simul_search_round_bucket_path,
     simul_sections_split_by_len_path,
 )
 from rmc.slack_creds import slack_token
-from rmc.utils.simultaneous_breaks import search_for_two_breaks
 
 logging.basicConfig(
     format="%(asctime)s (%(name)s %(lineno)s): %(message)s",
@@ -35,6 +43,321 @@ logging.basicConfig(
 )
 logger = logging.getLogger("run_batches")
 logger.setLevel(logging.INFO)
+
+
+RMC_PREFIX = "gs://regional_missense_constraint"
+"""
+Path to bucket attached to regional missense constraint (RMC) project.
+
+Adding this constant here to avoid ModuleNotFound errors in the
+PythonJobs. See `rmc.resources.basics` for full docstring.
+"""
+
+TEMP_PATH_WITH_FAST_DEL = "gs://gnomad-tmp-4day/rmc"
+"""
+Path to bucket for temporary files.
+
+Adding this constant here to avoid ModuleNotFound errors in the
+PythonJobs. See `rmc.resources.basics` for full docstring.
+"""
+
+SIMUL_BREAK_TEMP_PATH = f"{RMC_PREFIX}/temp/simul_breaks"
+"""
+Path to bucket to store temporary results for simultaneous searches.
+
+Adding this constant here to avoid ModuleNotFound errors in the
+PythonJobs. See `rmc.resources.basics` for full docstring.
+"""
+
+
+SIMUL_SEARCH_BUCKET_NAMES = {"prep", "raw_results", "final_results", "success_files"}
+"""
+Names of buckets nested within round bucket of `SIMUL_BREAK_TEMP_PATH`.
+
+Adding this constant here to avoid ModuleNotFound errors in the
+PythonJobs. See `rmc.resources.rmc` for full docstring.
+"""
+
+
+def simul_search_bucket_path(
+    search_num: Optional[int] = None,
+    freeze: int = CURRENT_FREEZE,
+) -> str:
+    """
+    Return path to bucket associated with simultaneous break search inputs and results.
+
+    Adding this constant here to avoid ModuleNotFound errors in the
+    PythonJobs. See `rmc.resources.rmc` for full docstring.
+
+    :param search_num: Search iteration number
+        (e.g., second round of searching for simultaneous break would be 2).
+        Default is None.
+    :param freeze: RMC freeze number. Default is CURRENT_FREEZE.
+    :return: Path to simultaneous break search round bucket.
+    """
+    return (
+        f"{SIMUL_BREAK_TEMP_PATH}/{freeze}/round{search_num}"
+        if search_num
+        else f"{SIMUL_BREAK_TEMP_PATH}/{freeze}"
+    )
+
+
+def simul_search_round_bucket_path(
+    search_num: int,
+    bucket_type: str,
+    bucket_names: Set[str] = SIMUL_SEARCH_BUCKET_NAMES,
+    freeze: int = CURRENT_FREEZE,
+) -> str:
+    """
+    Return path to bucket with  Tables resulting from a specific round of simultaneous break search.
+
+    Adding this constant here to avoid ModuleNotFound errors in the
+    PythonJobs. See `rmc.resources.rmc` for full docstring.
+
+    :param search_num: Search iteration number
+        (e.g., second round of searching for single break would be 2).
+    :param bucket_type: Bucket type.
+        Must be in `bucket_names`.
+    :param bucket_names: Possible bucket names for simultaneous search bucket type.
+        Default is `SIMUL_SEARCH_BUCKET_NAMES`.
+    :param freeze: RMC freeze number. Default is CURRENT_FREEZE.
+    :return: Path to a bucket in the simultaneous break search round bucket.
+    """
+    assert bucket_type in bucket_names, f"Bucket type must be one of {bucket_names}!"
+    return f"{simul_search_bucket_path(search_num, freeze)}/{bucket_type}"
+
+
+def get_obs_exp_expr(
+    obs_expr: hl.expr.Int64Expression,
+    exp_expr: hl.expr.Float64Expression,
+) -> hl.expr.Float64Expression:
+    """
+    Return observed/expected annotation based on inputs.
+
+    Typically imported from `constraint.py`. See `constraint.py` for full docstring.
+
+    :param hl.expr.Int64Expression obs_expr: Expression containing number of observed variants.
+    :param hl.expr.Float64Expression exp_expr: Expression containing number of expected variants.
+    :return: Observed/expected expression.
+    :rtype: hl.expr.Float64Expression
+    """
+    # Cap the o/e ratio at 1 to avoid pulling out regions that are enriched for missense variation
+    # Code is looking for missense constraint, so regions with a ratio of >= 1.0 can be grouped together
+    return hl.nanmin(obs_expr / exp_expr, 1)
+
+
+def get_dpois_expr(
+    oe_expr: hl.expr.Float64Expression,
+    obs_expr: hl.expr.Int64Expression,
+    exp_expr: hl.expr.Float64Expression,
+) -> hl.expr.StructExpression:
+    """
+    Calculate probability densities (natural log) of the observed values under a Poisson model.
+
+    Typically imported from `constraint.py`. See `constraint.py` for full docstring.
+
+    :param oe_expr: Expression of observed/expected value.
+    :param obs_expr: Expression containing observed variants count.
+    :param exp_expr: Expression containing expected variants count.
+    :return: Natural log of the probability density under Poisson model.
+    """
+    # log_p = True returns the natural logarithm of the probability density
+    return hl.dpois(obs_expr, exp_expr * oe_expr, log_p=True)
+
+
+def calculate_window_chisq(
+    max_idx: hl.expr.Int32Expression,
+    i: hl.expr.Int32Expression,
+    j: hl.expr.Int32Expression,
+    cum_obs: hl.expr.ArrayExpression,
+    cum_exp: hl.expr.ArrayExpression,
+    section_oe: hl.expr.Float64Expression,
+    min_num_exp_mis: hl.expr.Float64Expression,
+) -> hl.expr.Float64Expression:
+    """
+    Calculate chi square significance value for each possible simultaneous breaks window.
+
+    Used only when calculating simultaneous breaks.
+
+    Chi square formula: 2 * (hl.log(total_alt) - hl.log(total_null))
+
+    :param hl.expr.Int32Expression max_idx: Largest list index value.
+    :param hl.expr.Int32Expression i: Smaller list index value corresponding to the smaller position of the two break window.
+    :param hl.expr.Int32Expression j: Larger list index value corresponding to the larger position of the two break window.
+    :param hl.expr.ArrayExpression cum_obs: List containing cumulative observed missense values.
+    :param hl.expr.ArrayExpression cum_exp: List containing cumulative expected missense values.
+    :param hl.expr.Float64Expression section_oe: Transcript/transcript section overall observed/expected (OE) missense ratio.
+    :param hl.expr.Float64Expression min_num_exp_mis: Minimum expected missense value for all three windows defined by two possible
+        simultaneous breaks.
+    :return: Chi square significance value.
+    """
+    return hl.or_missing(
+        # Return missing when breakpoints do not create 3 windows
+        # (return missing when index is the smallest or largest position)
+        (i != 0) & (j != max_idx) &
+        # Return missing when any of the windows do not have the minimum
+        # number of expected missense variants
+        (cum_exp[i - 1] >= min_num_exp_mis)
+        & ((cum_exp[j] - cum_exp[i - 1]) >= min_num_exp_mis)
+        & ((cum_exp[-1] - cum_exp[j]) >= min_num_exp_mis),
+        (
+            2
+            * (
+                # Create alt distribution
+                (
+                    # Create alt distribution for section [start_pos, pos[i])
+                    # The missense values for this section are the cumulative values at
+                    # one index smaller than index i
+                    get_dpois_expr(
+                        oe_expr=get_obs_exp_expr(
+                            cum_obs[i - 1],
+                            hl.max(cum_exp[i - 1], 1e-09),
+                        ),
+                        obs_expr=cum_obs[i - 1],
+                        exp_expr=hl.max(cum_exp[i - 1], 1e-09),
+                    )
+                    # Create alt distribution for section [pos[i], pos[j]]
+                    # The missense values for this section are the cumulative values at index j
+                    # minus the cumulative values at index i -1
+                    + get_dpois_expr(
+                        oe_expr=get_obs_exp_expr(
+                            (cum_obs[j] - cum_obs[i - 1]),
+                            hl.max(cum_exp[j] - cum_exp[i - 1], 1e-09),
+                        ),
+                        obs_expr=cum_obs[j] - cum_obs[i - 1],
+                        exp_expr=hl.max(cum_exp[j] - cum_exp[i - 1], 1e-09),
+                    )
+                    # Create alt distribution for section (pos[j], end_pos]
+                    # The missense values for this section are the cumulative values at the last index
+                    # minus the cumulative values at index j
+                    + get_dpois_expr(
+                        oe_expr=get_obs_exp_expr(
+                            (cum_obs[-1] - cum_obs[j]),
+                            hl.max(cum_exp[-1] - cum_exp[j], 1e-09),
+                        ),
+                        obs_expr=cum_obs[-1] - cum_obs[j],
+                        exp_expr=hl.max(cum_exp[-1] - cum_exp[j], 1e-09),
+                    )
+                )
+                # Create null distribution
+                - (
+                    get_dpois_expr(
+                        oe_expr=section_oe,
+                        obs_expr=cum_obs[i - 1],
+                        exp_expr=hl.max(cum_exp[i - 1], 1e-09),
+                    )
+                    + get_dpois_expr(
+                        oe_expr=section_oe,
+                        obs_expr=cum_obs[j] - cum_obs[i - 1],
+                        exp_expr=hl.max(cum_exp[j] - cum_exp[i - 1], 1e-09),
+                    )
+                    + get_dpois_expr(
+                        oe_expr=section_oe,
+                        obs_expr=cum_obs[-1] - cum_obs[j],
+                        exp_expr=hl.max(cum_exp[-1] - cum_exp[j], 1e-09),
+                    )
+                )
+            )
+        ),
+    )
+
+
+def search_for_two_breaks(
+    group_ht: hl.Table,
+    count: int,
+    chisq_threshold: float = scipy.stats.chi2.ppf(1 - P_VALUE, 2),
+    min_num_exp_mis: float = MIN_EXP_MIS,
+    min_chisq_threshold: float = MIN_CHISQ_THRESHOLD,
+    save_chisq_ht: bool = False,
+    freeze: int = CURRENT_FREEZE,
+) -> hl.Table:
+    """
+    Search for transcripts/transcript sections with simultaneous breaks.
+
+    This function searches for breaks for all possible window sizes that exceed a minimum threshold of expected missense variants.
+
+    :param group_ht: Input Table aggregated by transcript/transcript section with lists of cumulative observed
+        and expected missense values. HT is filtered to contain only transcript/sections without
+        a single significant breakpoint.
+    :param count: Which transcript group is being run (based on counter generated in `main`).
+    :param chisq_threshold: Chi-square significance threshold. Default is
+        `scipy.stats.chi2.ppf(1 - P_VALUE, 2)`.
+        Default value used in ExAC was 13.8, which corresponds to a p-value of 0.001
+        with 2 degrees of freedom.
+        (https://www.itl.nist.gov/div898/handbook/eda/section3/eda3674.htm)
+    :param min_num_exp_mis: Minimum expected missense value for all three windows defined by two possible
+        simultaneous breaks.
+    :param min_chisq_threshold: Minimum chi square value to emit from search.
+        Default is MIN_CHISQ_THRESHOLD,
+        which corresponds to a p-value of 0.025 with 2 degrees of freedom.
+    :param save_chisq_ht: Whether to save HT with chi square values annotated for every locus
+        (as long as chi square value is >= min_chisq_threshold).
+        This saves a lot of extra data and should only occur during the initial search round.
+        Default is False.
+    :param freeze: RMC freeze number. Default is CURRENT_FREEZE.
+    :return: Table filtered to transcript/sections with significant simultaneous breakpoints
+        and annotated with breakpoint information.
+    """
+    # Create ArrayExpression of StructExpressions that stores each
+    # pair of positions (window breakpoints) and their corresponding chi square value
+    break_values_expr = hl.range(
+        group_ht.start_idx.i_start, group_ht.i_max_idx
+    ).flatmap(
+        lambda i: hl.range(group_ht.start_idx.j_start, group_ht.j_max_idx).map(
+            lambda j: hl.struct(
+                i=i,
+                j=j,
+                chisq=hl.nanmax(
+                    calculate_window_chisq(
+                        group_ht.max_idx,
+                        i,
+                        j,
+                        group_ht.cum_obs,
+                        group_ht.cum_exp,
+                        group_ht.section_oe,
+                        min_num_exp_mis,
+                    ),
+                    -1,
+                ),
+            )
+        )
+    )
+    # Filter ArrayExpression to only keep chi square values that are at least
+    # some minimum value (to shorten this array and save on storage)
+    break_values_expr = break_values_expr.filter(
+        lambda x: x.chisq >= min_chisq_threshold
+    )
+    group_ht = group_ht.annotate(break_values=break_values_expr)
+    # Extract the positions with the maximum chi square value (the "best" break)
+    group_ht = group_ht.annotate(
+        best_break=group_ht.break_values.fold(
+            lambda x, y: hl.if_else(x.chisq >= y.chisq, x, y),
+            hl.struct(i=-1, j=-1, chisq=-99),
+        )
+    )
+    group_ht = group_ht.transmute(
+        chisq=group_ht.best_break.chisq,
+        # Adjust breakpoint inclusive/exclusiveness to be consistent with single break breakpoints, i.e.
+        # so that the breakpoint site itself is the last site in the left subsection. Thus, the resulting
+        # subsections will be divided as follows:
+        # [section_start, first_break_pos] (first_break_pos, second_break_pos] (second_break_pos, section_end]
+        breakpoints=(
+            group_ht.positions[group_ht.best_break.i] - 1,
+            group_ht.positions[group_ht.best_break.j],
+        ),
+    )
+    group_ht_path = (
+        f"{TEMP_PATH_WITH_FAST_DEL}/freeze{freeze}_batch_temp_chisq_group{count}.ht"
+    )
+    if save_chisq_ht:
+        group_ht_path = (
+            f"{SIMUL_BREAK_TEMP_PATH}/freeze{freeze}/batch_temp_chisq_group{count}.ht"
+        )
+    group_ht = group_ht.checkpoint(group_ht_path, overwrite=True)
+
+    # Remove rows with maximum chi square values below the threshold
+    group_ht = group_ht.filter(group_ht.chisq >= chisq_threshold)
+    return group_ht
 
 
 def process_section_group(
@@ -277,7 +600,7 @@ def main(args):
     if not args.docker_image:
         logger.info("Picking default docker image...")
         args.docker_image = (
-            "us-central1-docker.pkg.dev/broad-mpg-gnomad/images/rmc_simul_search"
+            "us-central1-docker.pkg.dev/hailgenetics/hail:0.2.132-py3.10"
         )
         # NOTE: Python version on your local machine must match the Python version in this image
         logger.warning(
@@ -518,8 +841,7 @@ if __name__ == "__main__":
         "--docker-image",
         help="""
         Docker image to provide to hail Batch. Must have dill, hail, and python installed.
-        Suggested image: us-central1-docker.pkg.dev/broad-mpg-gnomad/images/rmc_simul_search.
-
+        Suggested image: hailgenetics/hail:0.2.132-py3.10.
         Default is None -- script will select default image if not specified on the command line.
         """,
         default=None,
