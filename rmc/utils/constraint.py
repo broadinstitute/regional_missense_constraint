742041318010119c2d20c2713834827c099b6ed7
ch-kr <kchao@broadinstitute.org>
2026-04-08 16:28:21 -0400

    add is_gray option as well

--- rmc/utils/constraint.py
+++ rmc/utils/constraint.py
@@ -2221,6 +2221,8 @@ def format_rmc_browser_ht(
             low_coverage=ht.low_coverage,
         )
     )
+    ht = ht.annotate(is_gray=(ht.region.p > P_VALUE) | ht.region.low_coverage)
+
     # Group Table by transcript
     ht = ht.group_by("transcript").aggregate(regions=hl.agg.collect(ht.region))
 
