ccb812878e4e7e934f2e209b0a81bad1c4034d7c
ch-kr <kchao@broadinstitute.org>
2026-04-15 15:57:59 -0400

    make globals consistent with docstring

--- rmc/utils/constraint.py
+++ rmc/utils/constraint.py
@@ -2118,16 +2118,19 @@ def add_globals_rmc_browser(
     outlier_transcripts = get_constraint_transcripts(
         filter_to_canonical=filter_to_canonical, outlier=True
     )
+    qc_pass_transcripts = all_transcripts.difference(outlier_transcripts)
 
     ht = ht.select_globals()
     return ht.annotate_globals(
         transcript_counts=hl.struct(
-            all_transcripts=all_transcripts.difference(outlier_transcripts),
+            all_transcripts=qc_pass_transcripts,
             rmc_transcripts=rmc_transcripts.difference(outlier_transcripts),
+            transcripts_no_rmc=qc_pass_transcripts.difference(rmc_transcripts),
         ),
         transcript_counts_all=hl.struct(
             all_transcripts=all_transcripts,
             rmc_transcripts=rmc_transcripts,
+            transcripts_no_rmc=all_transcripts.difference(rmc_transcripts),
             outlier_transcripts=outlier_transcripts,
         ),
     )
